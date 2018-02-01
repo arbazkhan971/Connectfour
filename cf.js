8ca2727cc49fb8bf5ce967a6617ee81e8dd43063
arbazkhan971 <31075248+arbazkhan971@users.noreply.github.com>
2018-02-01 22:12:29 +0530

    Update cf.js

--- cf.js
+++ cf.js
@@ -1,4 +1,3 @@
-// We need to use jQuery for the following:
 
 var player1 = prompt("Player One: Enter Your Name , you will be Blue");
 var player1Color = 'rgb(86, 151, 255)';
@@ -9,7 +8,7 @@ var player2Color = 'rgb(237, 45, 73)';
 var game_on = true;
 var table = $('table tr');
 
-// http://stackoverflow.com/questions/6139407/getting-td-by-index-with-jquery
+
 function reportWin(rowNum,colNum) {
   console.log("You won starting at this row,col");
   console.log(rowNum);
@@ -138,33 +137,4 @@ $('.board button').on('click',function() {
     $('h3').text(currentName+": it is your turn, please pick a column to drop your red chip.");
     currentColor = player2Color;
   }
-
 })
-
-
-
-// Helper function to help you understand Rows and Columns From A Table
-// http://stackoverflow.com/questions/788225/table-row-and-column-number-in-jquery
-//
-// $('.board button').on('click',function(){
-//   // This is the Column Number (starts at zero):
-//   console.log('This is the Column:');
-//   console.log($(this).closest("td").index());
-//   // This is the Row Number:
-//   console.log("This is the Row:");
-//   console.log($(this).closest("tr").index());
-//   console.log('\n');
-//   // This is a way to grab a particular cell (replace):
-//   // $('table').eq(rowIndex).find('td').eq(colIndex)
-// });
-
-// // Change color on click
-// $('.board button').on('click',function() {
-//   if($(this).css('background-color') === 'rgb(51, 51, 51)'){
-//     $(this).css('background-color','rgb(86, 151, 255)');
-//   }else if ($(this).css('background-color') === 'rgb(86, 151, 255)'){
-//     $(this).css('background-color','rgb(237, 45, 73)');
-//   }else{
-//     $(this).css('background-color','rgb(51, 51, 51)');
-//   }
-// });
